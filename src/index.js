7662ba6e7114500ea2064e05b277b8c7e2a4e2d6
f3liip3182 <feliignacio.fpv@gmail.com>
2025-06-01 14:05:09 -0400

    feat: auth controller y registro con verificación

--- src/index.js
+++ src/index.js
@@ -12,12 +12,16 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// ✅ Rutas principales
+// Rutas principales
 const reportesRouter = require('./routes/reportes');
 app.use('/api/reportes', reportesRouter);
 
-// ✅ Iniciar servidor
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+// Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🟢 Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
+
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,4 +5,5 @@ const { register } = require('../controllers/auth.controller');
 // Registro
 router.post('/register', register);
 
+
 module.exports = router;
